f47afa6cdbda7d25203e21fdf0bcf881bf0c131a
xeype <alexeypedko@gmail.com>
2023-02-27 18:37:30 +0200

    Add Customers model

--- realtor_app/models.py
+++ realtor_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
+class Customers(models.Model):
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    address = models.TextField()
