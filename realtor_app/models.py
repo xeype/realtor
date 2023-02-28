f778538374f30840f8aa1bcc568d7ad9b969a920
xeype <alexeypedko@gmail.com>
2023-02-28 02:37:11 +0200

    add Apartments and Agreements models

--- realtor_app/models.py
+++ realtor_app/models.py
@@ -23,3 +23,16 @@ class Prices(models.Model):
 class Services(models.Model):
     service_name = models.CharField(max_length=255)
     price = models.ForeignKey(Prices, on_delete=models.CASCADE)
+
+
+class Apartments(models.Model):
+    num_of_rooms = models.IntegerField()
+    address = models.CharField(max_length=255)
+    manager = models.ForeignKey(Employees, on_delete=models.CASCADE)
+    price = models.ForeignKey(Prices, on_delete=models.CASCADE)
+
+
+class Agreements(models.Model):
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    apartment = models.ForeignKey(Apartments, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employees, on_delete=models.CASCADE)
